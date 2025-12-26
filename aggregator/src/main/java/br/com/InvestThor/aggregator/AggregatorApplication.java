38b3c27896dee0a807f684bb70feab7d0ff9749c
Godinho-Matheus <matheus.fsgodinho@gmail.com>
2025-12-26 15:22:03 -0300

    Swagger e novos logs caso a api da Brapi caia.

--- aggregator/src/main/java/br/com/InvestThor/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/br/com/InvestThor/aggregator/AggregatorApplication.java
@@ -2,12 +2,25 @@ package br.com.InvestThor.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @SpringBootApplication
+@EnableCaching
 public class AggregatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AggregatorApplication.class, args);
 	}
 
+	@RestControllerAdvice
+	public class GlobalExceptionHandler {
+		@ExceptionHandler(Exception.class)
+		public ResponseEntity<String> handleException(Exception e) {
+			return ResponseEntity.status(500).body("Opa! Erro no servidor: " + e.getMessage());
+		}
+	}
+	
 }
--- aggregator/src/main/java/br/com/InvestThor/aggregator/service/StockService.java
+++ aggregator/src/main/java/br/com/InvestThor/aggregator/service/StockService.java
@@ -2,6 +2,7 @@ package br.com.InvestThor.aggregator.service;
 
 import br.com.InvestThor.aggregator.client.BrapiClient;
 import br.com.InvestThor.aggregator.client.dto.StockDto;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -15,14 +16,16 @@ public class StockService {
     public StockService(BrapiClient brapiClient) {
         this.brapiClient = brapiClient;
     }
-
+//Exemplos para teste: mglu3,SEQL3,IFCM3,JSLG3,OIBR3
+@Cacheable("stocks")
     public List<StockDto> getTopStocks(String tickers) {
         return Arrays.stream(tickers.split(","))
                 .parallel()
                 .map(ticker -> brapiClient.getBatchQuotes(ticker))
                 .flatMap(response -> response.results().stream())
-                .filter(stock -> stock.regularMarketChangePercent() != null && stock.regularMarketChangePercent() > 0)
+                .filter(stock -> stock.regularMarketChangePercent() != null && stock.regularMarketChangePercent() > -10)//Filtra as ações (coloquei 10 pra teste)
                 .sorted((a, b) -> Double.compare(b.regularMarketChangePercent(), a.regularMarketChangePercent()))
                 .toList();
     }
+
 }
